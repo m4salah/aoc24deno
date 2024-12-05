677bb28a81832f28f9f566e55771c416d0d94007
Mohamed A. Salah <me@msalah.net>
2024-12-06 00:42:11 +0200

    solve day3

--- day2/main.ts
+++ day2/main.ts
@@ -24,65 +24,6 @@ function isSafeReport(arr: number[]): boolean {
   return true;
 }
 
-// Tolerate a single bad level
-export function isSafeReportWithDampener(arr: number[], badLevel = 0): boolean {
-  const mode: "inc" | "dec" = arr[0] < arr[1] ? "inc" : "dec";
-
-  if (badLevel > 1) {
-    return false;
-  }
-
-  for (let i = 2; i < arr.length; i++) {
-    // Maintain the mode
-    const diff = arr[i] - arr[i - 1];
-    if (diff == 0) {
-      const removedI = [...arr.slice(0, i), ...arr.slice(i + 1)];
-      const removedI_1 = [...arr.slice(0, i - 1), ...arr.slice(i)];
-      const removedI_2 = [...arr.slice(0, i - 2), ...arr.slice(i - 1)];
-      return (
-        isSafeReportWithDampener(removedI, badLevel + 1) ||
-        isSafeReportWithDampener(removedI_1, badLevel + 1) ||
-        isSafeReportWithDampener(removedI_2, badLevel + 1)
-      );
-    }
-
-    if (mode == "dec" && diff > 0) {
-      const removedI = [...arr.slice(0, i), ...arr.slice(i + 1)];
-      const removedI_1 = [...arr.slice(0, i - 1), ...arr.slice(i)];
-      const removedI_2 = [...arr.slice(0, i - 2), ...arr.slice(i - 1)];
-      return (
-        isSafeReportWithDampener(removedI, badLevel + 1) ||
-        isSafeReportWithDampener(removedI_1, badLevel + 1) ||
-        isSafeReportWithDampener(removedI_2, badLevel + 1)
-      );
-    }
-
-    if (mode == "inc" && diff < 0) {
-      const removedI = [...arr.slice(0, i), ...arr.slice(i + 1)];
-      const removedI_1 = [...arr.slice(0, i - 1), ...arr.slice(i)];
-      const removedI_2 = [...arr.slice(0, i - 2), ...arr.slice(i - 1)];
-      return (
-        isSafeReportWithDampener(removedI, badLevel + 1) ||
-        isSafeReportWithDampener(removedI_1, badLevel + 1) ||
-        isSafeReportWithDampener(removedI_2, badLevel + 1)
-      );
-    }
-
-    if (Math.abs(diff) > 3) {
-      const removedI = [...arr.slice(0, i), ...arr.slice(i + 1)];
-      const removedI_1 = [...arr.slice(0, i - 1), ...arr.slice(i)];
-      const removedI_2 = [...arr.slice(0, i - 2), ...arr.slice(i - 1)];
-      return (
-        isSafeReportWithDampener(removedI, badLevel + 1) ||
-        isSafeReportWithDampener(removedI_1, badLevel + 1) ||
-        isSafeReportWithDampener(removedI_2, badLevel + 1)
-      );
-    }
-  }
-
-  return true;
-}
-
 export function safeReportsCounter(input: string) {
   const lines = input.split("\n");
   let result = 0;
