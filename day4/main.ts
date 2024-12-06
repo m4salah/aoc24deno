22e10d1dbf05816c5e751d53a8f4140be09ed714
Mohamed A. Salah <me@msalah.net>
2024-12-06 16:39:30 +0200

    solve part2

--- day4/main.ts
+++ day4/main.ts
@@ -74,44 +74,32 @@ export function countXMAS(input: string): number {
   const rows = input.split("\n");
   const n_row = rows.length;
   const n_col = rows[0].length;
-  rows.forEach(function (entry) {
-    console.log(entry);
-  });
-
   let result = 0;
   for (let y = 0; y < n_row; y++) {
     for (let x = 0; x < n_col; x++) {
       if (rows[y][x] == "X") {
         if (isFoundForward(rows, x, y)) {
-          console.log("found X forward", x, y);
           result += 1;
         }
         if (isFoundBackward(rows, x, y)) {
-          console.log("found X backward", x, y);
           result += 1;
         }
         if (isFoundAbove(rows, x, y)) {
-          console.log("found X above", x, y);
           result += 1;
         }
         if (isFoundBottom(rows, x, y)) {
-          console.log("found X bottom", x, y);
           result += 1;
         }
         if (isAboveFoward(rows, x, y)) {
-          console.log("found X above foward", x, y);
           result += 1;
         }
         if (isAboveBackward(rows, x, y)) {
-          console.log("found X above backward", x, y);
           result += 1;
         }
         if (isBottomForward(rows, x, y)) {
-          console.log("found X bottom forward", x, y);
           result += 1;
         }
         if (isBottomBackward(rows, x, y)) {
-          console.log("found X bottom backward", x, y);
           result += 1;
         }
       }
@@ -120,7 +108,119 @@ export function countXMAS(input: string): number {
   return result;
 }
 
+function isAboveFowardX(
+  rows: string[],
+  x: number,
+  y: number,
+): [boolean, number, number] {
+  return [
+    rows[y][x] == "M" &&
+      rows[y - 1]?.[x + 1] == "A" &&
+      rows[y - 2]?.[x + 2] == "S",
+    x + 1,
+    y - 1,
+  ];
+}
+
+function isAboveBackwardX(
+  rows: string[],
+  x: number,
+  y: number,
+): [boolean, number, number] {
+  return [
+    rows[y][x] == "M" &&
+      rows[y - 1]?.[x - 1] == "A" &&
+      rows[y - 2]?.[x - 2] == "S",
+    x - 1,
+    y - 1,
+  ];
+}
+
+function isBottomForwardX(
+  rows: string[],
+  x: number,
+  y: number,
+): [boolean, number, number] {
+  return [
+    rows[y][x] == "M" &&
+      rows[y + 1]?.[x + 1] == "A" &&
+      rows[y + 2]?.[x + 2] == "S",
+    x + 1,
+    y + 1,
+  ];
+}
+
+function isBottomBackwardX(
+  rows: string[],
+  x: number,
+  y: number,
+): [boolean, number, number] {
+  return [
+    rows[y][x] == "M" &&
+      rows[y + 1]?.[x - 1] == "A" &&
+      rows[y + 2]?.[x - 2] == "S",
+    x - 1,
+    y + 1,
+  ];
+}
+export function countX_MAS(input: string): number {
+  const rows = input.split("\n");
+  const n_row = rows.length;
+  const n_col = rows[0].length;
+  rows.forEach(function (entry) {
+    console.log(entry);
+  });
+
+  const mapA = new Map<string, number>();
+  for (let y = 0; y < n_row; y++) {
+    for (let x = 0; x < n_col; x++) {
+      if (rows[y][x] == "M") {
+        const isAboveFoward = isAboveFowardX(rows, x, y);
+        const isAboveBackward = isAboveBackwardX(rows, x, y);
+        const isBottomForward = isBottomForwardX(rows, x, y);
+        const isBottomBackward = isBottomBackwardX(rows, x, y);
+        if (isAboveFoward[0]) {
+          const k = mapA.get(`${isAboveFoward[1]},${isAboveFoward[2]}`);
+          if (k) {
+            mapA.set(`${isAboveFoward[1]},${isAboveFoward[2]}`, k + 1);
+          } else {
+            mapA.set(`${isAboveFoward[1]},${isAboveFoward[2]}`, 1);
+          }
+        }
+        if (isAboveBackward[0]) {
+          const k = mapA.get(`${isAboveBackward[1]},${isAboveBackward[2]}`);
+          if (k) {
+            mapA.set(`${isAboveBackward[1]},${isAboveBackward[2]}`, k + 1);
+          } else {
+            mapA.set(`${isAboveBackward[1]},${isAboveBackward[2]}`, 1);
+          }
+        }
+
+        if (isBottomForward[0]) {
+          const k = mapA.get(`${isBottomForward[1]},${isBottomForward[2]}`);
+          if (k) {
+            mapA.set(`${isBottomForward[1]},${isBottomForward[2]}`, k + 1);
+          } else {
+            mapA.set(`${isBottomForward[1]},${isBottomForward[2]}`, 1);
+          }
+        }
+
+        if (isBottomBackward[0]) {
+          const k = mapA.get(`${isBottomBackward[1]},${isBottomBackward[2]}`);
+          if (k) {
+            mapA.set(`${isBottomBackward[1]},${isBottomBackward[2]}`, k + 1);
+          } else {
+            mapA.set(`${isBottomBackward[1]},${isBottomBackward[2]}`, 1);
+          }
+        }
+      }
+    }
+  }
+  console.log(mapA);
+  return Array.from(mapA.values().filter((v) => v == 2)).length;
+}
+
 if (import.meta.main) {
-  const result = countXMAS(await Deno.readTextFile("./input.txt"));
+  const result = countX_MAS(await Deno.readTextFile("./input.txt"));
   console.log(result);
 }
